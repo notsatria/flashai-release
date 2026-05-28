d7c07ec499c7434f5b89704e8aa2d9b82508c4ed
notsatria <buanasatriaa@gmail.com>
2026-05-28 18:23:27 +0700

    chore: rollback package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.notsatria.flashcard"
+    namespace = "com.notsatria.starter"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -13,7 +13,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.notsatria.flashcard"
+        applicationId = "com.notsatria.starter"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
