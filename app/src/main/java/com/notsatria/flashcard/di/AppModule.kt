e59c8e74951ffdc82c4ca09174d109975d2f6f34
notsatria <buanasatriaa@gmail.com>
2026-05-30 15:44:31 +0700

    feat: register firebase dependencies

--- app/src/main/java/com/notsatria/flashcard/di/AppModule.kt
+++ app/src/main/java/com/notsatria/flashcard/di/AppModule.kt
@@ -1,9 +1,21 @@
 package com.notsatria.flashcard.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.notsatria.flashcard.data.repository.FirebaseAuthRepository
+import com.notsatria.flashcard.data.repository.FirebaseDeckRepository
+import com.notsatria.flashcard.domain.repository.AuthRepository
+import com.notsatria.flashcard.domain.repository.DeckRepository
 import com.notsatria.flashcard.navigation.AppNavigator
 import com.notsatria.flashcard.navigation.Navigator
 import org.koin.dsl.module
 
 val appModule = module {
     single<Navigator> { AppNavigator() }
+
+    single<FirebaseAuth> { FirebaseAuth.getInstance() }
+    single<FirebaseFirestore> { FirebaseFirestore.getInstance() }
+
+    single<AuthRepository> { FirebaseAuthRepository(get()) }
+    single<DeckRepository> { FirebaseDeckRepository(get(), get()) }
 }
